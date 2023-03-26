bc0b8f68b5a72b845e4270fc857a1b20cf289fe6
Polina <polinakiseleva4@gmail.com>
2023-03-26 12:16:17 +0200

    Написала тесты с использованием unittest

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,14 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src import stack
+
+
+class TestStack(unittest.TestCase):
+    test_stack = stack.Stack()
+
+    def test_push_to_stack(self):
+        self.test_stack.push('test')
+        self.assertEqual(self.test_stack, self.test_stack)
+
+    def test_pop_from_stack(self):
+        self.test_stack.pop()
+        self.assertEqual(self.test_stack, self.test_stack)
