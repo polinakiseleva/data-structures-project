a87f71022e6f3adf9c0d032268a9b5ac6a3eaa87
Polina <polinakiseleva4@gmail.com>
2023-04-01 17:48:31 +0200

    Переписала тест на метод pop

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -10,5 +10,6 @@ class TestStack(unittest.TestCase):
         self.assertEqual(self.test_stack, self.test_stack)
 
     def test_pop_from_stack(self):
-        self.test_stack.pop()
-        self.assertEqual(self.test_stack, self.test_stack)
+        self.test_stack.push('data1')
+        data = self.test_stack.pop()
+        self.assertEqual(data, 'data1')
