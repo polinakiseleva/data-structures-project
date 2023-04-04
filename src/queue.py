a8f3414c4f29ded0fb3d054f28d94561ba18a804
Polina <polinakiseleva4@gmail.com>
2023-04-04 12:03:16 +0200

    Реализовала класс Node Классу Queue добавила методы __init__, enqueue и __str__

--- src/queue.py
+++ src/queue.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Queue:
@@ -15,7 +16,8 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.head = None
+        self.tail = None
 
     def enqueue(self, data):
         """
@@ -23,16 +25,29 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        pass
+        new_node = Node(data, None)
 
-    def dequeue(self):
-        """
-        Метод для удаления элемента из очереди. Возвращает данные удаленного элемента
+        if self.tail is not None:
+            self.tail.next_node = new_node
+        else:
+            self.head = new_node
+        self.tail = new_node
 
-        :return: данные удаленного элемента
-        """
-        pass
+    # def dequeue(self):
+    #     """
+    #     Метод для удаления элемента из очереди. Возвращает данные удаленного элемента
+    #
+    #     :return: данные удаленного элемента
+    #     """
+    #     pass
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        pass
+        result = []
+        node = self.head
+
+        while node is not None:
+            result.append(str(node.data))
+            node = node.next_node
+
+        return "\n".join(result)
