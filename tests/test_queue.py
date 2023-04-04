fb9302f9b81c93baf98951d7bc62ea812006ee1e
Polina <polinakiseleva4@gmail.com>
2023-04-04 12:18:01 +0200

    Добавила тесты для метода enqueue и магического метода __str__ класса Queue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+from src import queue
+
+
+class TestStack(unittest.TestCase):
+
+    def test_enqueue(self):
+        test_queue = queue.Queue()
+        test_queue.enqueue('data1')
+        test_queue.enqueue('data2')
+        self.assertEqual(test_queue.head.data, 'data1')
+        self.assertEqual(test_queue.head.next_node.data, 'data2')
+
+    def test_dunder_str_for_queue(self):
+        test_queue = queue.Queue()
+        test_queue.enqueue('data1')
+        test_queue.enqueue('data2')
+        self.assertEqual(str(test_queue), "data1\ndata2")
